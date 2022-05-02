ab87578ed1c16f0b0d98051a26dc4be65b8c2e34
joaocarvalho19 <joao.carvalho19@hotmail.com>
2022-05-02 21:00:19 +0100

    first commit

---
+++ AppGui/AppGui/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using mmisharp;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AppGui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MmiCommunication mmiC;
+
+        //  new 16 april 2020
+        private MmiCommunication mmiSender;
+        private LifeCycleEvents lce;
+        private MmiCommunication mmic;
+
+        String chess_url = "https://www.chess.com/play/computer";
+
+        IWebDriver driver;
+
+        [SetUp]
+        public void start_Browser()
+        {
+            // Local Selenium WebDriver
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(chess_url);
+
+            
+            IWebElement okButton = driver.FindElement(By.XPath("//button[.='Ok']"));
+            okButton.Click();
+        }
+
+        public MainWindow()
+        {
+            /*InitializeComponent();
+
+
+            mmiC = new MmiCommunication("localhost",8000, "User1", "GUI");
+            mmiC.Message += MmiC_Message;
+            mmiC.Start();
+
+            // NEW 16 april 2020
+            //init LifeCycleEvents..
+            lce = new LifeCycleEvents("APP", "TTS", "User1", "na", "command"); // LifeCycleEvents(string source, string target, string id, string medium, string mode
+            // MmiCommunication(string IMhost, int portIM, string UserOD, string thisModalityName)
+            mmic = new MmiCommunication("localhost", 8000, "User1", "GUI");*/
+            start_Browser();
+
+        }
+
+        private void MmiC_Message(object sender, MmiEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+            var doc = XDocument.Parse(e.Message);
+            var com = doc.Descendants("command").FirstOrDefault().Value;
+            dynamic json = JsonConvert.DeserializeObject(com);
+
+            Shape _s = null;
+            switch ((string)json.recognized[0].ToString())
+            {
+                case "SQUARE": _s = rectangle;
+                    break;
+                case "CIRCLE": _s = circle;
+                    break;
+                case "TRIANGLE": _s = triangle;
+                    break;
+            }
+
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                switch ((string)json.recognized[1].ToString())
+                {
+                    case "GREEN":
+                        _s.Fill = Brushes.Green;
+                        break;
+                    case "BLUE":
+                        _s.Fill = Brushes.Blue;
+                        break;
+                    case "RED":
+                        _s.Fill = Brushes.Red;
+                        break;
+                }
+            });
+
+            //  new 16 april 2020
+            mmic.Send(lce.NewContextRequest());
+
+            string json2 = ""; // "{ \"synthesize\": [";
+            json2 += (string)json.recognized[0].ToString()+ " ";
+            json2 += (string)json.recognized[1].ToString() + " DONE." ;
+            //json2 += "] }";
+            /*
+             foreach (var resultSemantic in e.Result.Semantics)
+            {
+                json += "\"" + resultSemantic.Value.Value + "\", ";
+            }
+            json = json.Substring(0, json.Length - 2);
+            json += "] }";
+            */
+            var exNot = lce.ExtensionNotification(0 + "", 0 + "", 1, json2);
+            mmic.Send(exNot);
+
+
+        }
+    }
+}
